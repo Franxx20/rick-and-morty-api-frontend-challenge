f0d6ff5d714f115dfe6037c41cddbc3baafd7c3f
Fran <fgarcete@alumno.unlam.edu.ar>
2024-08-20 15:34:05 -0300

    added character cards to details pages

--- src/pages/EpisodeDetailsPage.tsx
+++ src/pages/EpisodeDetailsPage.tsx
@@ -1,34 +1,50 @@
 import {useLocation} from "react-router-dom";
-import type {Episode} from "../utils/types.ts";
+import type {Character, Episode} from "../utils/types.ts";
 import {Title} from "../components/title.tsx";
+import CharacterCard from "../components/CharacterCard.tsx";
+import {useEffect, useState} from "react";
+import {getCharactersByID} from "../utils/api.ts";
+import {NavBar} from "../components/NavBar.tsx";
 
 export function EpisodeDetailsPage() {
     const location = useLocation()
     const episode: Episode = location.state || {}
+    const characterIds: number[] = episode.characters.map((character) => Number(character.split('/').pop()))
+    const [characters, setCharacters] = useState<Character[]>([]);
+
+    useEffect(() => {
+
+        const fetchCharacters = async () => {
+            const result = await getCharactersByID(characterIds)
+            setCharacters(result)
+        }
+
+        fetchCharacters()
+    }, []);
+
     return (
-        <div className="container mx-auto p-6">
+        <div className="container mx-auto">
+            <div className="sticky top-0 z-50 mb-4">
+                <NavBar/>
+            </div>
             <div className="bg-white shadow-lg rounded-lg p-6">
                 <Title title={'Episode Details'}></Title>
                 <div className="space-y-4 text-lg">
                     <div><span className="font-semibold">ID:</span> {episode?.id}</div>
                     <div><span className="font-semibold">Name:</span> {episode?.name}</div>
-                    <div><span className="font-semibold">Created:</span> {episode?.created}</div>
                     <div><span className="font-semibold">Air Date:</span> {episode?.air_date}</div>
                 </div>
                 <div className="mt-6">
                     <h2 className="text-xl font-semibold mb-2">Characters:</h2>
-                    <ul className="list-disc list-inside space-y-1">
-                        {episode?.characters.map((character, index) => (
-                            <li key={index} className="text-blue-500 hover:underline">
-                                {character}
-                            </li>
+
+                    <div className="columns-1 sm:columns-2 lg:columns-3 py-10 md:py-20 gap-4">
+                        {characters.map((character) => (
+                            <div key={character.id} className="mb-4 break-inside-avoid">
+                                <CharacterCard data={character}/>
+                            </div>
                         ))}
-                    </ul>
-                </div>
-                <div className="mt-6">
-                    <div><span className="font-semibold">URL:</span> <a href={episode?.url}
-                                                                        className="text-blue-500 hover:underline">{episode?.url}</a>
                     </div>
+
                 </div>
             </div>
         </div>
--- src/pages/LocationDetailsPage.tsx
+++ src/pages/LocationDetailsPage.tsx
@@ -1,13 +1,32 @@
 import {useLocation} from "react-router-dom";
-import type {Location} from '../utils/types.ts';
+import type {Character, Location} from '../utils/types.ts';
 import {Title} from "../components/title.tsx";
+import {NavBar} from "../components/NavBar.tsx";
+import {useEffect, useState} from "react";
+import {getCharactersByID} from "../utils/api.ts";
+import CharacterCard from "../components/CharacterCard.tsx";
 
 export function LocationDetailsPage() {
     const locationObject = useLocation()
     const location: Location = locationObject.state || {}
+    const characterIds: number[] = location.residents.map((character) => Number(character.split('/').pop()))
+    const [characters, setCharacters] = useState<Character[]>([]);
+
+    useEffect(() => {
+
+        const fetchCharacters = async () => {
+            const result = await getCharactersByID(characterIds)
+            setCharacters(result)
+        }
+
+        fetchCharacters()
+    }, []);
 
     return (
-        <div className="container mx-auto p-6">
+        <div className="container mx-auto">
+            <div className="sticky top-0 z-50 mb-4">
+                <NavBar/>
+            </div>
             <div className="bg-white shadow-lg rounded-lg p-6">
                 <Title title={'Location Details'}></Title>
                 <div className="space-y-4 text-lg">
@@ -18,21 +37,25 @@ export function LocationDetailsPage() {
                     </div>
                     <div><span className="font-semibold">Dimension:</span> {location?.dimension || 'Unknown'}</div>
                 </div>
-                <div className="mt-6">
-                    <h2 className="text-xl font-semibold mb-2">Residents:</h2>
-                    <ul className="list-disc list-inside space-y-1">
-                        {location?.residents.map((character, index) => (
-                            <li key={index} className="text-blue-500 hover:underline">
-                                {character}
-                            </li>
-                        ))}
-                    </ul>
+
+                <h2 className="text-xl font-semibold mt-4">Residents:</h2>
+                <div className="columns-1 sm:columns-2 lg:columns-3 md:py-20 gap-4">
+                    {characters.map((character) => (
+                        <div key={character.id} className="mb-4 break-inside-avoid">
+                            <CharacterCard data={character}/>
+                        </div>
+                    ))}
                 </div>
-                <div className="mt-6">
-                    <div><span className="font-semibold">URL:</span> <a href={location?.url}
-                                                                        className="text-blue-500 hover:underline">{location?.url}</a>
-                    </div>
-                </div>
+                {/*<div className="mt-6">*/}
+                {/*    <h2 className="text-xl font-semibold mb-2">Residents:</h2>*/}
+                {/*    <ul className="list-disc list-inside space-y-1">*/}
+                {/*        {location?.residents.map((character, index) => (*/}
+                {/*            <li key={index} className="text-blue-500 hover:underline">*/}
+                {/*                {character}*/}
+                {/*            </li>*/}
+                {/*        ))}*/}
+                {/*    </ul>*/}
+                {/*</div>*/}
             </div>
         </div>
     );
